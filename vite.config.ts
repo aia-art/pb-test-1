192ba6ca3707a16deaafb2b047ece1e4c4537de2
aia-art <anshytuber@gmail.com>
2026-05-25 17:27:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // Change 'prompt-battle' to match your GitHub repo name
-const BASE = process.env.GITHUB_ACTIONS ? '/prompt-battle/' : '/';
+const BASE = process.env.GITHUB_ACTIONS ? '/pb-test-1/' : '/';
 
 export default defineConfig({
   plugins: [react()],
